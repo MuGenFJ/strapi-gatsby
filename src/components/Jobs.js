7188d787b9a1794804082ccaff54c38d13c9a3aa
MuGenFJ <63530527+MuGenFJ@users.noreply.github.com>
2020-11-12 15:46:30 +0100

    experiences sectection

--- src/components/Jobs.js
+++ src/components/Jobs.js
@@ -4,8 +4,70 @@ import { FaAngleDoubleRight } from "react-icons/fa"
 import { graphql, useStaticQuery } from "gatsby"
 import { Link } from "gatsby"
 
+const query = graphql`
+  {
+    allStrapiJobs(sort: {order: DESC, fields: strapiId}) {
+      nodes {
+        strapiId
+        company
+        date
+        position
+        desc {
+          id
+          name
+        }
+      }
+    }
+  }
+`
+
 const Jobs = () => {
-  return <h2>jobs component</h2>
+  const data = useStaticQuery(query);
+  const { allStrapiJobs: { nodes: jobs } } = data
+  const [value, setValue] = React.useState(0)
+  const { company, position, date, desc } = jobs[value]
+
+  return (
+    <section className="section jobs">
+      <Title title="experience" />
+      <div className="jobs-center">
+        {/* btn container */}
+        <div className="btn-container">
+          {jobs.map((item, index) => {
+            return (
+              // if index == value then add also an active class
+              <button
+                key={item.strapiId}
+                onClick={() => setValue(index)}
+                className={`job-btn ${index === value && 'active-btn'}`}
+              >
+                {item.company}
+              </button>
+            )
+          })}
+        </div>
+        {/* job info */}
+        <active className="job-info">
+          <h3>{position}</h3>
+          <h4>{company}</h4>
+          <p className="job-date">{date}</p>
+          {
+            desc.map((item) => {
+              return (
+                <div key={item.id} className="job-desc">
+                  <FaAngleDoubleRight className="job-icon"></FaAngleDoubleRight>
+                  <p>{item.name }</p>
+                </div>
+              )
+            })
+          }
+        </active>
+      </div>
+      <Link to="/about" className="btn center-btn">
+          more info
+      </Link>
+    </section>
+  )
 }
 
 export default Jobs
